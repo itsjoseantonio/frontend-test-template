0802626bd3604c93e904f127c92c64660e3256f3
Jose Antonio Valga <jos.valga@gmail.com>
2025-11-13 22:43:45 -0500

    feat: Create button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from "react";
+import clsx from "clsx";
+
+interface ButtonProps {
+  onClick: () => void;
+  variant?: Variant;
+  children: ReactNode;
+  className?: string;
+}
+
+const buttonVariants = {
+  default: "text-white bg-[#585660]",
+  outline: "text-[#585660] bg-white",
+};
+
+type Variant = keyof typeof buttonVariants;
+
+const Button = ({
+  children,
+  onClick,
+  variant = "default",
+  className,
+}: ButtonProps) => {
+  return (
+    <button
+      className={clsx(
+        `border rounded-md py-2 px-2 w-full cursor-pointer`,
+        buttonVariants[variant],
+        className
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
