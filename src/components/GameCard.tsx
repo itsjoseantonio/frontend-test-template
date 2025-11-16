254abf99f29c885025a199575813a2b3e8e1e08e
Jose Antonio Valga <jos.valga@gmail.com>
2025-11-16 10:29:01 -0500

    refactor: Update component card name

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,25 +1,36 @@
 "use client";
+import { useEffect } from "react";
 
 import { Game } from "@/utils/endpoint";
 import Button from "./ui/Button";
 import Badge from "./ui/Badge";
+import getProducts from "@/services/product.service";
 
-const ProductCard = () => {
+const GameCard = ({ name, image, genre, isNew, price }: Game) => {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await getProducts("/api/games");
+      console.log(res, "res client");
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="border border-[#8F8F8F] rounded-xl p-4 flex flex-col gap-4">
       <div className="relative rounded-t-xl">
         <img
-          src="/game-images/cyberpunk2077.jpeg"
+          src={image}
           alt="awd"
           className="rounded-t-xl w-full max-h-[240px]"
         />
-        <Badge className="absolute left-2 top-2">New</Badge>
+        {isNew && <Badge className="absolute left-2 top-2">New</Badge>}
       </div>
       <div className="flex flex-col gap-2">
         <span className="text-[#737373]">GENRE</span>
         <div className="flex justify-between font-bold">
-          <h3>Product name</h3>
-          <p>$ 119</p>
+          <h3>{name}</h3>
+          <p>{`$${price}`}</p>
         </div>
         <Button onClick={() => console.log("raaaaa")} variant="outline">
           ADD TO CART
@@ -29,4 +40,4 @@ const ProductCard = () => {
   );
 };
 
-export default ProductCard;
+export default GameCard;
